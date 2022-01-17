6ba52f2c4337542f41e86a6845da530fd1155654
3jun <kk3june@gmail.com>
2022-01-17 11:48:10 +0900

    #2.9 타입스크립트의 꽃 🌷 Type Alias

--- lecture2-basic-types/1-4-alias.ts
+++ lecture2-basic-types/1-4-alias.ts
@@ -2,27 +2,29 @@
   /**
    * Type Aliases
    */
+  // type alias 를 사용하면 직접 type을 정의할 수 있다.
   type Text = string;
-  const name: Text = 'ellie';
-  const address: Text = 'korea';
+  const name: Text = "ellie";
+  const address: Text = "korea";
   type Num = number;
+  // ✅ 원시 타입 뿐만 아니라 object 형태도 정의할 수 있다.
   type Student = {
     name: string;
     age: number;
   };
   const student: Student = {
-    name: 'ellie',
+    name: "ellie",
     age: 12,
   };
 
   /**
    * String Literal Types
    */
-  type Name = 'name';
+  type Name = "name";
   let ellieName: Name;
-  ellieName = 'name';
-  type JSON = 'json';
-  const json: JSON = 'json';
+  ellieName = "name";
+  type JSON = "json";
+  const json: JSON = "json";
 
   type Boal = true;
 }
