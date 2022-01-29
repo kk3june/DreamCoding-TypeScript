786c092cab6043c2aa735155205fc1028a2bc80f
3jun <kk3june@gmail.com>
2022-01-29 17:30:29 +0900

    # 4.12 Interface 인터페이스, 모든 것의 시작

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -8,7 +8,15 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
-  class CoffeeMachine implements CoffeeMaker {
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  //CoffeeMachine 클래스는 CoffeeMaker와 CommercialCoffeeMaker 2가지 인터페이스 모두를 따른다.
+  //현재 CoffeeMachine 클래스에서는 위에 CommercialCoffeMaker에서 추가한 clean API가 빠져있으므로 추가
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     private static BEANS_GRAMM_PER_SHOT: number = 7;
     private coffeeBeans: number = 0; // instance (object) level
 
@@ -16,8 +24,12 @@
       this.coffeeBeans = coffeeBeans;
     }
 
-    static makeMachine(coffeeBeans: number): CoffeeMaker {
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
       return new CoffeeMachine(coffeeBeans);
+    }
+
+    clean() {
+      console.log('cleaning the machine...');
     }
 
     fillCoffeeBeans(beans: number) {
@@ -55,7 +67,37 @@
     }
   }
 
-  const maker: CoffeeMaker = CoffeeMachine.makeMachine(32);
-  // maker.fillCoffeeBeans(32);  => 사용할 수 없다.
-  maker.makeCoffee(2);
+  // 이렇게 인터페이스로 다시 타입을 제한해서 받게되면 인터페이스에서 정의된 아이들만 사용할 수 있다.
+  const maker2: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  maker2.fillCoffeeBeans(32);
+  maker2.makeCoffee(2);
+  maker2.clean();
+
+  // 예제
+  // CoffeeMaker 인터페이스를 생성자에서 받아오고
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  // CommercialCoffeeMaker 라는 인터페이스를 생성자에서 받아온다.
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  // makeMachine을 이용하면 CoffeeMachine 라는 오브젝트를 리턴하는데
+  // CoffeeMachine 이라는 타입으로 오브젝트를 받게되면 오브젝트 안에 있는 퍼블릭 함수들은 다 접근 가능
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+  amateur.makeCoffee();
+  pro.makeCoffee();
 }
