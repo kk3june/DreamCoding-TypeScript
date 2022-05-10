28273a6ee594075736aec12e927eda7835a0292d
3jun <kk3june@gmail.com>
2022-05-10 16:43:55 +0900

    :sparkles: PageComponent 구현

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,11 +1,8 @@
 import { PageComponent } from "./components/page.js";
-
 class App {
-  private readonly content: PageComponent;
-  constructor(appRoot: HTMLElement) {
+  constructor(appRoot) {
     this.content = new PageComponent();
     this.content.attachTo(appRoot);
   }
 }
-
-new App(document.querySelector(".contents") as HTMLElement);
+new App(document.querySelector(".contents"));
