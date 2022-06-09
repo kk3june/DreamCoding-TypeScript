27bcdc659c7604052e17108babcef334655e01f0
3jun <kk3june@gmail.com>
2022-06-09 16:03:13 +0900

    :sparkles: PageItemComponent, PageComponent 및 Composable 인터페이스 구현

--- motion-lecture/src/components/page/page.ts
+++ motion-lecture/src/components/page/page.ts
@@ -1,7 +1,42 @@
-import { BaseComponent } from './../component.js';
+import { BaseComponent, Component } from './../component.js';
 
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
   constructor() {
-    super('<ul class="page">This is PageComponenet!</ul>');
+    super(`<li class="page-item">
+            <section class="page-item__body">
+              <div class="page-item__controls">
+                <button class="close">&times;</button>
+              </div>
+            </section>
+          </li>`);
+  }
+  // 외부에서 어떤 아이템을 전달하는지에 따라 위 section에 추가할 api
+  addChild(child: Component) {
+    const container = this.element.querySelector(
+      '.page-item__body'
+    )! as HTMLElement;
+    // Component에 있는 attachTo 인터페이스 사용하여 child를 원하는 곳에 붙일 수 있다.
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
+  constructor() {
+    super('<ul class="page"></ul>');
+  }
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 }
