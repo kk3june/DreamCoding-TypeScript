02700533d2b2c530da70001d736bee6c3d46f9ce
3jun <kk3june@gmail.com>
2022-07-14 11:56:09 +0900

    :recycle: 리팩토링 위한 함수 생성
    공통코드는 함수 내부, 차이점 있는 것들은 외부인자로 받아올 수 있도록

--- motion-lecture/src/app.ts
+++ motion-lecture/src/app.ts
@@ -12,9 +12,15 @@ import {
 } from './components/page/page.js';
 import { Component } from './components/component.js';
 
+// 1-1) 1)의 InputComponent 인자를 받아오기 위해 타입 설정
+type InputComponentConstructor<T = MediaSectionInput | TextSectionInput> = {
+  // 아무것도 전달받지 않는 new 라는 생성사를 가지고 있다.
+  new (): T;
+};
+
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
@@ -82,6 +88,32 @@ class App {
         const image = new TodoComponent(inputSection.title, inputSection.body);
         this.page.addChild(image);
         dialog.removeFrom(dialogRoot);
+      });
+    });
+  }
+
+  private bindElementToDialog<T extends MediaSectionInput | TextSectionInput>(
+    selector: string,
+    InputComponent: InputComponentConstructor<T>
+  ) {
+    // 여기서 셀렉터는 어떤 타입이냐에 따라 달라질 수 있으므로 seletor 인자로 받아온다.
+    // 버튼 역시 어떤 것인지 알 수 없으므로 imageBtn -> element로 수정
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      // image, video, task, todo 에 따라 들어가는 input이 달라진다.1)
+      // 1-3) 앞선 단계가 모두 끝나면 MediaSection -> InputComponent
+      const inputSection = new InputComponent();
+      dialog.addChild(inputSection);
+      dialog.attachTo(this.dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(this.dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        const image = new ImageComponent(inputSection.title, inputSection.url);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
       });
     });
   }
