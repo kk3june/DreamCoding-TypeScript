03b7466726da9ac2dc928f1733d7a43e24cd4687
3jun <kk3june@gmail.com>
2022-07-14 16:18:23 +0900

    :recycle: bindElementToDialog 함수사용하여 중복되는 코드 삭제

--- motion-lecture/src/app.ts
+++ motion-lecture/src/app.ts
@@ -12,9 +12,7 @@ import {
 } from './components/page/page.js';
 import { Component } from './components/component.js';
 
-// 1-1) 1)의 InputComponent 인자를 받아오기 위해 타입 설정
 type InputComponentConstructor<T = MediaSectionInput | TextSectionInput> = {
-  // 아무것도 전달받지 않는 new 라는 생성사를 가지고 있다.
   new (): T;
 };
 
@@ -24,94 +22,47 @@ class App {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
-    imageBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const inputSection = new MediaSectionInput();
-      dialog.addChild(inputSection);
-      dialog.attachTo(dialogRoot);
-
-      dialog.setOnCloseListener(() => {
-        dialog.removeFrom(dialogRoot);
-      });
-      dialog.setOnSubmitListener(() => {
-        const image = new ImageComponent(inputSection.title, inputSection.url);
-        this.page.addChild(image);
-        dialog.removeFrom(dialogRoot);
-      });
-    });
-
-    const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
-    videoBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const inputSection = new MediaSectionInput();
-      dialog.addChild(inputSection);
-      dialog.attachTo(dialogRoot);
-
-      dialog.setOnCloseListener(() => {
-        dialog.removeFrom(dialogRoot);
-      });
-      dialog.setOnSubmitListener(() => {
-        const image = new VideoComponent(inputSection.title, inputSection.url);
-        this.page.addChild(image);
-        dialog.removeFrom(dialogRoot);
-      });
-    });
-    const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
-    noteBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const inputSection = new TextSectionInput();
-      dialog.addChild(inputSection);
-      dialog.attachTo(dialogRoot);
-
-      dialog.setOnCloseListener(() => {
-        dialog.removeFrom(dialogRoot);
-      });
-      dialog.setOnSubmitListener(() => {
-        const image = new NoteComponent(inputSection.title, inputSection.body);
-        this.page.addChild(image);
-        dialog.removeFrom(dialogRoot);
-      });
-    });
-
-    const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
-    todoBtn.addEventListener('click', () => {
-      const dialog = new InputDialog();
-      const inputSection = new TextSectionInput();
-      dialog.addChild(inputSection);
-      dialog.attachTo(dialogRoot);
-
-      dialog.setOnCloseListener(() => {
-        dialog.removeFrom(dialogRoot);
-      });
-      dialog.setOnSubmitListener(() => {
-        const image = new TodoComponent(inputSection.title, inputSection.body);
-        this.page.addChild(image);
-        dialog.removeFrom(dialogRoot);
-      });
-    });
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-image',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
+    );
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-video',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-note',
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-todo',
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
   }
 
   private bindElementToDialog<T extends MediaSectionInput | TextSectionInput>(
     selector: string,
-    InputComponent: InputComponentConstructor<T>
+    InputComponent: InputComponentConstructor<T>,
+    // makeSection 인자는 callback 함수, input 을 전달해주면 그것을 사용하여 어떤 Component를 만듦 1)
+    makeSection: (input: T) => Component
   ) {
-    // 여기서 셀렉터는 어떤 타입이냐에 따라 달라질 수 있으므로 seletor 인자로 받아온다.
-    // 버튼 역시 어떤 것인지 알 수 없으므로 imageBtn -> element로 수정
     const element = document.querySelector(selector)! as HTMLButtonElement;
     element.addEventListener('click', () => {
       const dialog = new InputDialog();
-      // image, video, task, todo 에 따라 들어가는 input이 달라진다.1)
-      // 1-3) 앞선 단계가 모두 끝나면 MediaSection -> InputComponent
-      const inputSection = new InputComponent();
-      dialog.addChild(inputSection);
+      const input = new InputComponent();
+      dialog.addChild(input);
       dialog.attachTo(this.dialogRoot);
 
       dialog.setOnCloseListener(() => {
         dialog.removeFrom(this.dialogRoot);
       });
       dialog.setOnSubmitListener(() => {
-        const image = new ImageComponent(inputSection.title, inputSection.url);
+        // 1) makeSection 함수를 호출하면서 전달받은 input을 전달
+        const image = makeSection(input);
         this.page.addChild(image);
         dialog.removeFrom(this.dialogRoot);
       });
